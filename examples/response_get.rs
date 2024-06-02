a8a3a31d2f88b1c641679e46c10fad28680d1475
hirotake111 <hirotake111@users.noreply.github.com>
2024-06-03 06:25:09 +0900

    add HTTP response parser

---
+++ examples/response_get.rs
@@ -0,0 +1,8 @@
+use rust_http_parser::http::HTTPResponse;
+
+fn main() {
+    let input = std::fs::read_to_string("examples/response_get.txt").unwrap();
+    let response = input.parse::<HTTPResponse>().unwrap();
+    println!("====\n{input}\n====\n");
+    dbg!(response);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -48,20 +48,10 @@ pub mod http {
 
         fn from_str(s: &str) -> Result<Self, Self::Err> {
             let mut iterator = s.split(" ").into_iter();
-            let method = iterator
+            let method: Method = iterator
                 .next()
                 .ok_or("failed to get HTTP method".to_string())
-                .and_then(|method| match method {
-                    "GET" | "get" => Ok(Method::GET),
-                    "POST" | "post" => Ok(Method::POST),
-                    "PUT" | "put" => Ok(Method::PUT),
-                    "DELETE" | "delete" => Ok(Method::DELETE),
-                    "HEAD" | "head" => Ok(Method::HEAD),
-                    "OPTIONS" | "options" => Ok(Method::OPTIONS),
-                    "CONNECT" | "connect" => Ok(Method::CONNECT),
-                    "TRACE" | "trace" => Ok(Method::TRACE),
-                    _ => Err(format!("invalid HTTP method: {method}")),
-                })?;
+                .and_then(|method| method.parse())?;
             let request_target = iterator
                 .next()
                 .ok_or("failed to get request target".to_string())?
@@ -116,14 +106,113 @@ pub mod http {
         TRACE,
     }
 
+    impl FromStr for Method {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "GET" | "get" => Ok(Method::GET),
+                "POST" | "post" => Ok(Method::POST),
+                "PUT" | "put" => Ok(Method::PUT),
+                "DELETE" | "delete" => Ok(Method::DELETE),
+                "HEAD" | "head" => Ok(Method::HEAD),
+                "OPTIONS" | "options" => Ok(Method::OPTIONS),
+                "CONNECT" | "connect" => Ok(Method::CONNECT),
+                "TRACE" | "trace" => Ok(Method::TRACE),
+                _ => Err(format!("invalid HTTP method: {s}")),
+            }
+        }
+    }
+
+    #[derive(Debug, Clone)]
     pub struct HTTPResponse {
         status_line: StatusLine,
-        headers: HTTPHeaders,
-        body: Option<Vec<u8>>,
+        headers: Option<HTTPHeaders>,
+        body: Option<String>,
     }
+
+    impl FromStr for HTTPResponse {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut iterator = s.split("\r\n").peekable();
+            let status_line = iterator
+                .next()
+                .ok_or("failed to get status line".to_string())
+                .and_then(|s| s.parse::<StatusLine>())?;
+            let headers = if iterator.peek().is_some() {
+                Some(HTTPHeaders::new(&mut iterator)?)
+            } else {
+                None
+            };
+            let body = if iterator.peek().is_some() {
+                Some(iterator.collect())
+            } else {
+                None
+            };
+            Ok(HTTPResponse {
+                status_line,
+                headers,
+                body,
+            })
+        }
+    }
+
+    #[derive(Debug, Clone)]
     pub struct StatusLine {
-        http_version: String,
-        status_code: u16,
+        http_version: HTTPVersion,
+        status_code: StatusCode,
         status_text: String,
+    }
+
+    impl FromStr for StatusLine {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut iterator = s.split(" ");
+            let http_version: HTTPVersion = iterator
+                .next()
+                .ok_or("failed to get HTTP version".to_string())
+                .and_then(|s| s.parse())?;
+            let status_code = iterator
+                .next()
+                .ok_or("no status code to be parsed".to_string())
+                .and_then(|s| s.parse::<StatusCode>())?;
+            let status_text = iterator
+                .next()
+                .ok_or("failed to get status text".to_string())?
+                .to_string();
+            Ok(StatusLine {
+                http_version,
+                status_code,
+                status_text,
+            })
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    struct HTTPVersion(String);
+    impl FromStr for HTTPVersion {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            if s.starts_with("HTTP/") {
+                Ok(HTTPVersion(s.to_string()))
+            } else {
+                Err("invalid HTTP Version".to_string())
+            }
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    struct StatusCode(u16);
+    impl FromStr for StatusCode {
+        type Err = String;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            s.parse::<u16>()
+                .or(Err("failed parsing status code".to_string()))
+                .map(|n| StatusCode(n))
+        }
     }
 }
